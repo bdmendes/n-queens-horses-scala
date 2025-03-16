1e8d2884f676b27a7de0e0bf57781540d5a02c2e
Bruno Mendes <bd_mendes@outlook.com>
2025-03-16 21:09:35 +0000

    Add nqueens leetcode

---
+++ n-queens-leetcode/src/main/scala/solution/Main.scala
@@ -0,0 +1,19 @@
+package solution
+
+def solveNQueens(n: Int): List[List[String]] = {
+  Search.search(Board.empty(n), Piece.Queen, n).map(_.toStr)
+}
+
+object Solution extends App {
+  val board =
+    Board
+      .empty(4)
+      .set(FromTopLeftPosition(0, 0), Piece.Queen)
+      .set(FromTopLeftPosition(1, 2), Piece.Queen)
+  println(board.toStr)
+  println(board.pieces)
+  println(board.isValid)
+
+  val result = solveNQueens(9)
+  println(result)
+}
